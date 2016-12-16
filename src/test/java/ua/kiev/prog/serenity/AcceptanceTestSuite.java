b1a481feb3f60f1b3f868c0ae9c022955e74fb54
Gravituss <zaretskyyy@gmail.com>
2016-12-16 20:44:54 +0200

    initial commit

---
+++ src/test/java/ua/kiev/prog/serenity/AcceptanceTestSuite.java
@@ -0,0 +1,5 @@
+package ua.kiev.prog.serenity;
+
+import net.serenitybdd.jbehave.SerenityStories;
+
+public class AcceptanceTestSuite extends SerenityStories {}
---
+++ src/test/java/ua/kiev/prog/serenity/package-info.java
@@ -0,0 +1,8 @@
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+package ua.kiev.prog.serenity;
